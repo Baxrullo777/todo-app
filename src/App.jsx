576947f83284f78ee4649275a770bd3c813f5cde
Baxrullo777 <baxrullo37@gmail.com>
2024-05-13 10:44:32 +0500

    todo

---
+++ src/App.jsx
@@ -0,0 +1,109 @@
+import { useState, useEffect, useRef } from "react";
+import "./App.css";
+import Card from "./components/Card/card";
+import Input from "./components/Input/input";
+import DoneCard from "./components/DoneCard/done";
+import { motion } from "framer-motion";
+
+function App() {
+  const [tasks, setTasks] = useState(() => {
+    const localTasks = localStorage.getItem("taskLocalStorage");
+    return localTasks ? JSON.parse(localTasks) : [];
+  });
+
+  const [done, setDone] = useState(() => {
+    const localDone = localStorage.getItem("doneLocalStorage");
+    return localDone ? JSON.parse(localDone) : [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem("taskLocalStorage", JSON.stringify(tasks));
+  }, [tasks]);
+
+  useEffect(() => {
+    localStorage.setItem("doneLocalStorage", JSON.stringify(done));
+  }, [done]);
+
+  const addTask = (newTask) => {
+    const taskWithId = { id: Date.now(), text: newTask };
+    setTasks((prevTasks) => [...prevTasks, taskWithId]);
+  };
+
+  const handleRemove = (taskId) => {
+    setTasks((prevTasks) => prevTasks.filter((task) => task.id !== taskId));
+  };
+
+  const doneHandleRemove = (taskId) => {
+    setDone((prevDone) => prevDone.filter((task) => task.id !== taskId));
+  };
+
+  const timeoutRef = useRef();
+
+  const handleCheck = (taskId) => {
+    setTimeout(() => {
+      setTasks((prevTasks) => {
+        const newTasks = prevTasks.filter((task) => task.id !== taskId);
+        const completedTask = prevTasks.find((task) => task.id === taskId);
+  
+        if (completedTask) {
+          setDone((prevDone) => {
+
+            const isAlreadyDone = prevDone.find((task) => task.id === completedTask.id);
+            return isAlreadyDone ? prevDone : [...prevDone, completedTask];
+          });
+        }
+  
+        return newTasks;
+      });
+    }, 1000);
+  };
+  
+
+  useEffect(() => {
+    return () => {
+      clearTimeout(timeoutRef.current);
+    };
+  }, []);
+
+  return (
+    <>
+      <h1>TODO APP</h1>
+      <main>
+        <Input addTask={addTask} />
+        <div className="cards-main">
+          <span>Tasks</span>
+          <div className="cards">
+            {tasks.map((task, index) => (
+              <motion.div
+                initial={{ opacity: 0, scale: 0.5 }}
+                animate={{ opacity: 1, scale: 1 }}
+                transition={{ duration: 0.3, delay: index * 0.3 }}
+                key={task.id}  
+              >
+                <Card
+                  text={task.text}
+                  onRemove={() => handleRemove(task.id)}
+                  onCheck={() => handleCheck(task.id)}
+                />
+              </motion.div>
+            ))}
+          </div>
+        </div>
+        <div className="done-main">
+          <span>Done</span>
+          <div className="done">
+            {done.map((task) => (
+              <DoneCard
+                key={task.id}
+                text={task.text}
+                onRemove={() => doneHandleRemove(task.id)}
+              />
+            ))}
+          </div>
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Input/input.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+
+import plusImage from "../../assets/plus.svg";
+import "./input.css";
+
+const Input = ({ addTask }) => {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleChange = (e) => {
+
+      setInputValue(e.target.value);
+    
+  };
+
+  const handleAdd = () => {
+    if (inputValue.trim()) {
+      addTask(inputValue);
+      setInputValue("");
+    }
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter') {  
+      handleAdd();  
+    }
+  };
+  return (
+    <div className="input-main">
+      <input
+        type="text"
+        className="input"
+        placeholder="Add a new task"
+        value={inputValue}
+        onChange={handleChange}
+        onKeyDown={handleKeyPress}
+      />
+      <button className="input-button" onClick={handleAdd}>
+        <img src={plusImage} alt="" />
+      </button>
+    </div>
+  );
+};
+
+export default Input;
